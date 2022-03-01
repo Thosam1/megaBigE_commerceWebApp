1b9bf8652055ecdf5f9493be631695378dee3ff4
Thosam1 <thosamtenzin2002@gmail.com>
2022-03-01 20:31:26 +0100

    cart reducer + toggle show/hide functionality

--- src/components/header/header.component.jsx
+++ src/components/header/header.component.jsx
@@ -5,12 +5,13 @@ import { connect } from 'react-redux';
 import { auth } from '../../firebase/firebase.utils';
 
 import CartIcon from '../cart-icon/cart-icon.component';
+import CartDropdown from '../cart-dropdown/cart-dropdown.component';
 
 import { ReactComponent as Logo } from '../../assets/crown.svg'; // speci
 
 import './header.styles.scss';
 
-const Header = ( {currentUser} ) => ( // deconstructing the user we're passing from App.js
+const Header = ( {currentUser, hidden} ) => ( // deconstructing the user we're passing from App.js
     <div className='header'>
         <Link className='logo-container' to="/">
             <Logo className='logo' />
@@ -34,11 +35,19 @@ const Header = ( {currentUser} ) => ( // deconstructing the user we're passing f
             <CartIcon />
 
         </div>
+        {
+            hidden ?
+            null
+            :
+            <CartDropdown />
+        }
+        
     </div>
 );
 
-const mapStateToProps = state => ({ // state is top-level root reducer
-    currentUser: state.user.currentUser
+const mapStateToProps = ({user: {currentUser}, cart: {hidden}}) => ({ // state is top-level root reducer
+    currentUser,
+    hidden
 });
 
 export default connect(mapStateToProps)(Header);
---
+++ src/redux/cart/cart.types.js
@@ -0,0 +1,10 @@
+const CartActionTypes = {
+    TOGGLE_CART_HIDDEN: 'TOGGLE_CART_HIDDEN'
+  };
+  
+  export default CartActionTypes;
+
+
+
+
+  
