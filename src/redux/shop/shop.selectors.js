39f19a111b07e6f84c26ff8d0ef634e0ffacf893
Thosam1 <thosamtenzin2002@gmail.com>
2022-03-07 15:54:30 +0100

    selectors for directory+shop +collection component

--- src/redux/shop/shop.selectors.js
+++ src/redux/shop/shop.selectors.js
@@ -1,9 +1,23 @@
 import { createSelector } from "reselect";
 
+import memoize from "lodash.memoize";
+
 const selectShop = state => state.shop;
+
 
 export const selectCollections = createSelector(
     [selectShop],
     shop => shop.collections
 );
 
+export const selectCollectionsForPreview = createSelector(
+    [selectCollections],
+    collections => Object.keys(collections)
+)
+
+export const selectCollection = memoize((collectionUrlParam) =>
+    createSelector(
+        [selectCollections],
+        collections => Object.keys(collections).map(key => collections[key])
+    )
+); // memoize avoid reruning if function was already ran with same parameters
