d08b00a08dfb6999821ad8f631e1a0c991e10001
Thosam1 <thosamtenzin2002@gmail.com>
2022-03-08 16:34:52 +0100

    package json deleted for heroku + shop bug solved

--- src/components/collection-preview/collection-preview.component.jsx
+++ src/components/collection-preview/collection-preview.component.jsx
@@ -1,4 +1,3 @@
-
 import React from 'react';
 import { withRouter } from 'react-router-dom';
 
@@ -6,22 +5,25 @@ import CollectionItem from '../collection-item/collection-item.component';
 
 import './collection-preview.styles.scss';
 
-const CollectionPreview = ({ title, items, history, match }) => (
-  <div className='collection-preview'>
-    <h1
-      className='title'
-      onClick={() => history.push(`${match.path}/${title.toLowerCase()}`)}
-    >
-      {title.toUpperCase()}
-    </h1>
-    <div className='preview'>
-      {items
-        .filter((item, idx) => idx < 4)
-        .map(item => (
-          <CollectionItem key={item.id} item={item} />
-        ))}
+const CollectionPreview = ({ title, items, history, match }) => {
+
+  return (
+    <div className='collection-preview'>
+      <h1
+        className='title'
+        onClick={() => history.push(`${match.path}/${title.toLowerCase()}`)}
+      >
+        {title.toUpperCase()}
+      </h1>
+      <div className='preview'>
+        {items
+          .filter((item, idx) => idx < 4)
+          .map(item => (
+            <CollectionItem key={item.id} item={item} />
+          ))}
+      </div>
     </div>
-  </div>
-);
+  );
+}
 
 export default withRouter(CollectionPreview);
--- src/pages/shop/shop.component.jsx
+++ src/pages/shop/shop.component.jsx
@@ -1,3 +1,4 @@
+
 import React from 'react';
 import { Route } from 'react-router-dom';
 
@@ -5,13 +6,12 @@ import CollectionsOverview from '../../components/collections-overview/collectio
 import CollectionPage from '../collection/collection.component';
 
 const ShopPage = ({ match }) => {
-    return (<div className='shop-page'>
-        <Route exact path={`${match.path}`} component={CollectionsOverview} />
-        <Route path={`${match.path}/:collectionId`} component={CollectionPage} />
-    </div>);
+  return (
+    <div className='shop-page'>
+      <Route exact path={`${match.path}`} component={CollectionsOverview} />
+      <Route path={`${match.path}/:collectionId`} component={CollectionPage} />
+    </div>
+  );
 }
 
-       
 export default ShopPage;
-  
-
